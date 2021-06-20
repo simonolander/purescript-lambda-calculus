36f6dc2c039ae9e92a82b5fdc1edb05c1d469e28
Simon Olander <simon.olander@r2m.se>
2021-06-20 19:22:06 +0200

    Improve tests

---
+++ src/Lambda/Parser.purs
@@ -0,0 +1,66 @@
+module Lambda.Parser (term) where
+
+import Prelude hiding (between)
+import Control.Alt ((<|>))
+import Control.Lazy (fix)
+import Lambda.Expression (Expression(..))
+import Text.Parsing.Parser (Parser)
+import Text.Parsing.Parser.Combinators (chainl1)
+import Text.Parsing.Parser.String (char, oneOf)
+import Text.Parsing.Parser.Token (GenLanguageDef(..), LanguageDef, TokenParser, alphaNum, letter, makeTokenParser)
+
+clDef :: LanguageDef
+clDef =
+  LanguageDef
+    { commentStart: ""
+    , commentEnd: ""
+    , commentLine: ""
+    , nestedComments: false
+    , identStart: letter
+    , identLetter: alphaNum
+    , opStart: oneOf [ '.', '\\' ]
+    , opLetter: oneOf [ '.', '\\' ]
+    , reservedNames: [ "lambda", "λ" ]
+    , reservedOpNames: [ "." ]
+    , caseSensitive: false
+    }
+
+tokenParser :: TokenParser
+tokenParser = makeTokenParser clDef
+
+dot :: Parser String Char
+dot = char '.'
+
+variable :: Parser String Expression
+variable = do
+  id <- tokenParser.identifier
+  pure $ Variable id
+
+application :: Parser String Expression -> Parser String Expression
+application parser = do
+  fun <- parser
+  arg <- parser
+  pure $ Application fun arg
+
+lambda :: Parser String String
+lambda = tokenParser.symbol "λ"
+
+nonApplication :: Parser String Expression
+nonApplication =
+  fix \_ ->
+    tokenParser.parens term
+      <|> abstraction term
+      <|> variable
+
+abstraction :: Parser String Expression -> Parser String Expression
+abstraction parser = do
+  void lambda
+  var <- tokenParser.identifier
+  void dot
+  body <- parser
+  pure $ Abstraction var body
+
+term :: Parser String Expression
+term = do
+  tokenParser.whiteSpace
+  chainl1 nonApplication (pure Application)
